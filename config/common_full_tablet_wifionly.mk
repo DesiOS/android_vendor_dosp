1d40db22e79c08c698e3754c9030725257a5bccb
Kukun Mishra <kukun7610@gmail.com>
2017-08-29 23:24:35 +0530

    this is DOSP

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,9 +1,9 @@
 # Inherit common VIPER stuff
-$(call inherit-product, vendor/viper/config/common_full.mk)
+$(call inherit-product, vendor/dosp/config/common_full.mk)
 
 # Required VIPER packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include VIPER LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/viper/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/dosp/overlay/dictionaries
