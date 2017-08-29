205ca0838d6002003c3ecd239c111f235ae2b189
Kukun Mishra <kukun7610@gmail.com>
2017-08-29 23:24:05 +0530

    this is DOSP

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,4 +1,4 @@
 # Inherit common VIPER stuff
-$(call inherit-product, vendor/viper/config/common_full.mk)
+$(call inherit-product, vendor/dosp/config/common_full.mk)
 
 PRODUCT_PACKAGES += TvSettings
