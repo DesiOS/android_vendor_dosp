5a39dbb0c4b1ebb7853032ee7ee653d95f292fb6
Kukun Mishra <kukun7610@gmail.com>
2017-08-29 22:11:14 +0530

    This is DOSP

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,5 +1,5 @@
 # Inherit common VIPER stuff
-$(call inherit-product, vendor/viper/config/common_mini.mk)
+$(call inherit-product, vendor/dosp/config/common_mini.mk)
 
 # Required VIPER packages
 PRODUCT_PACKAGES += \
