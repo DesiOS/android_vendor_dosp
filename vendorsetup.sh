c42fbed39617c2ba0c0304827ca0b1bebe382e12
Kukun Mishra <kukun7610@gmail.com>
2017-08-31 15:45:48 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(curl -s https://raw.githubusercontent.com/ViperOS-Devices/devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\":".*?"' | sed -e 's/codename//;s/\"//g;s/\://')
+for device in $(curl -s https://raw.githubusercontent.com/DOSP-Project/official_devices/n7x/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\":".*?"' | sed -e 's/codename//;s/\"//g;s/\://')
 do
-add_lunch_combo viper_$device-userdebug
+add_lunch_combo dosp_$device-userdebug
 done
